76d02df9a2a7edb9cc53bc67f24b4a4eaeea07b9
Armel Wonga <armel.g.ngassa@studmail.w-hs.de>
2021-07-20 10:34:03 +0200

    changing message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1> Testing GitHub Pages with React and GitHub Actions</h1>
         </p>
         <a
           className="App-link"
